446b8e8b68af95382e318588a0f79ea803842b67
Didarul Alam <didarulalamrafi1312@gmail.com>
2026-06-24 23:46:08 +0600

    somthing added

--- index.js
+++ index.js
@@ -239,6 +239,8 @@ async function run() {
       }
     });
 
+    app.post();
+
     app.post("/jwt", (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "7d" });
